338dfbfcf72a249fdab3dfff81c2d65c821c886c
Xac47 <hasbulat69@gmail.com>
2024-06-04 16:50:10 +0300

    add file gigachat_api.py

---
+++ gigachat_api.py
@@ -0,0 +1,56 @@
+import uuid
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import requests
+
+from requests.auth import HTTPBasicAuth
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+SECRET = os.getenv('SECRET')
+
+
+def get_access_token() -> str:
+    url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'RqUID': str(uuid.uuid4()),
+    }
+    payload = {"scope": "GIGACHAT_API_PERS"}
+    response = requests.post(
+        url=url,
+        headers=headers,
+        auth=HTTPBasicAuth(CLIENT_ID, SECRET),
+        data=payload,
+        verify=False,
+    )
+    access_token = response.json()['access_token']
+
+    return access_token
+
+
+def send_prompt(msg: str, access_token: str):
+    url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
+
+    payload = json.dumps({
+        "model": "GigaChat",
+        "messages": [
+            {
+                "role": "user",
+                "content": msg
+            }
+        ]
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    response = requests.post(url, headers=headers, data=payload, verify=False)
+
+    return response.json()['choices'][0]['message']['content']
